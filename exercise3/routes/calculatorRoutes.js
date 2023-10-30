8f49a7cf6863758100664d4d06880aba60103097
DavidVerdugo7 <117783389+DavidVerdugo7@users.noreply.github.com>
2023-10-30 14:31:32 +1300

    commit exercise 3

---
+++ exercise3/routes/calculatorRoutes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+
+// suma
+router.get("/add", (req, res) => {
+  console.log(req.query);
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const result = num1 + num2;
+
+  res.status(200).json({ result: result });
+
+  // res.send("result = " + result);
+});
+
+//resta
+
+router.get("/subtract", (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let result = number1 - number2;
+  console.log(result);
+  res.status(200);
+  res.json({ result: result });
+});
+
+//multiplica
+router.get("/multiply", (req, res) => {
+  console.log(req.query);
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const result = num1 * num2;
+
+  res.status(200);
+  res.json({ result: result });
+});
+
+//dividir
+
+router.get("/divide", (req, res) => {
+  console.log(req.query);
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  const result = num1 / num2;
+
+  res.status(200);
+  res.json({ result: result });
+});
+
+module.exports = router;
